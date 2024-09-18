c211bb35c7ea93b8752a45bbed724593d17f3ec7
Molly McKenzie <160646541+mollymckenzie@users.noreply.github.com>
2024-09-17 21:20:51 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { MyButtonGrid } from "./index";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Songbirds of Tennessee</h1>
+      <MyButtonGrid />
+    </div>
+  );
+}
